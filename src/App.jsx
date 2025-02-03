6027dda50fe378db69ab5157ef609ae1d8c96f74
Madi@Fz <imadmrrouch21@gmail.com>
2025-02-03 20:54:34 +0100

    import pkm

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import pokemons from './assets/pokemons'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  console.log(pokemons)
 
   return (
     <>
